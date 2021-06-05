6bc087326983504535039d78952aebd516cc9f12
Wesllem <74308320+wesllemsilva1985@users.noreply.github.com>
2021-06-05 07:18:13 -0300

    Update win-hcxInstall.py
    corrected font color formatting code

--- win-hcxInstall.py
+++ win-hcxInstall.py
@@ -5,15 +5,15 @@
 #Site: https://nsworld.com.br
 #Github: https://github.com.br/igorMatsunaga
 
-#Edited by Wellem
-#code adapted for console windows
-#Github https://github.com/wesllemsilva1985 and https://github.com/Wescomp-Dev
+#Edited by: Wellem
+#Modified source code for terminal windows
+#Github: https://github.com/wesllemsilva1985 and https://github.com/Wescomp-Dev
 
 import os
-
+os.system('') #enable VT100 Escape Sequence for WINDOWS 10 Ver. 1607
     
     
-logo = '''
+logo = '\033[1;34m'+'''
 
 ██╗  ██╗ ██████╗██╗  ██╗██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗     
 ██║  ██║██╔════╝╚██╗██╔╝██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║     
@@ -23,15 +23,16 @@ logo = '''
 ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝                                                                                  
 -----------------------------------coded by igorMatsunaga----------------------------------------
 -----------------------------------------nsworld-------------------------------------------------
-'''
+'''+'\033[39m'
 print(logo)
-menu = '''
+
+menu = '\033[1;33m'+'''
 Escolha o número da opção que deseja utilizar:
 
 (1)  Instalar Hcxdumptool
 (2)  Instalar hxctools
 (3)  Instalar Hcxdumptool e hxctools
-'''
+'''+'\033[39m'
 print(menu)
 
 def dumptool():
@@ -44,17 +45,17 @@ def tools():
     os.system("apt-get update && apt-get install zlib1g-dev && apt-get install libpcap0.8-dev && apt-get install libcurl4-openssl-dev")
     os.system("cd hcxtools && make && make install")
 
-escolha = input("Digite a opção: ")
+escolha = input('\033[1;32m'+'''Digite a opção: '''+'\033[0;0m')
 
 
 if(escolha == "1"):
-    print("Instalando Hcxdumptool ")
+    print('\033[1;33m'+'''Instalando Hcxdumptool '''+'\033[0;0m')
     dumptool()
 elif(escolha == "2"):
-    print("Instalando hxctools")
+    print('\033[1;33m'+'''Instalando hxctools'''+'\033[0;0m')
     tools()
 elif(escolha == "3"):
-    print("As ferramentas estão sendo instaladas")
+    print('\033[1;33m'+'''As ferramentas estão sendo instaladas'''+'\033[0;0m')
     dumptool()
     tools()    
 else:
